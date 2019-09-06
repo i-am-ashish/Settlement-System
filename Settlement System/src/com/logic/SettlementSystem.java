a1aa45808509fbe522af6da8b30dc0bb107c9285
i-am-ashish <ashish1996nov@gmail.com>
2019-09-06 22:25:58 +0530

    Update SettlementSystem.java

--- Settlement System/src/com/logic/SettlementSystem.java
+++ Settlement System/src/com/logic/SettlementSystem.java
@@ -5,13 +5,15 @@ import java.util.HashMap;
 public interface SettlementSystem {
 
 
-	public HashMap<String,Integer> getClientID(String clientName);
-	public HashMap<String,Integer> getSecurity(String securityName);
+	public HashMap<String,Integer> getParticipantIdMap();
+	public HashMap<String,Integer> getSecurityIdMap();
 	public void performNettingProcedure(HashMap<Integer,HashMap<Integer,Integer>> map);
 	public void generateObligationReport();
 	public void detectShortages();
+	public boolean performSettlement();//update the account balance from the netted table 
 	public void handleCorporateAction(String coporateAction);
 	public void generateCostofSelletmentReceipt();
+
 	
 	
 }
