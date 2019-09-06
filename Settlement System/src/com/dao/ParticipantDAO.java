ebfe997a42cbac2af6c14a2f691b00b0bcc7c2b6
i-am-ashish <ashish1996nov@gmail.com>
2019-09-06 22:20:05 +0530

    Update ParticipantDAO.java

--- Settlement System/src/com/dao/ParticipantDAO.java
+++ Settlement System/src/com/dao/ParticipantDAO.java
@@ -4,21 +4,26 @@ import java.util.List;
 
 import com.pojo.Participant;
 import com.pojo.Share;
+package com.dao;
+
+import java.util.List;
+
+import com.pojo.Participant;
+import com.pojo.Security;
 
 public interface ParticipantDAO {
 
-	////////////////////////////////////////////////////////////////////////////////////////////
-	public List<Share> getAllBalance(int participantId); //existing balance of funds and shares
-	public List<Share> getClientNetValues(int participantId); //netted value of funds and shares
-	public void updateBalance(String ClientID,List<Share> listOfShares);//
-	public boolean updateParticipant(Participant participant);
-	public boolean addParticipant(Participant participant);
-	public boolean addLoginInfo(String username, String password);
-	public boolean updateLoginInfo(int participantId, String password);
-	public Participant getParticipantById(int participantId);
-	public Integer getLoginInfo(String username, String password); //login validation from db
-	public String getParticipantNameById(int participantId);	//For mapping clientId to its Name for UI
-	public List<String> getAllParticipantName();
-
+		public List<Security> getAllBalance(int participantId); //existing balance of funds and shares
+		public List<Security> getClientNetValues(int participantId); //netted value of funds and shares
+		public void updateBalance(String ClientID,List<Security> listOfShares);//
+		public boolean updateParticipant(Participant participant);
+		public boolean addParticipant(Participant participant);
+		public boolean addLoginInfo(String username, String password);
+		public boolean updateLoginInfo(int participantId, String password);
+		public Participant getParticipantById(int participantId);
+		public Integer getLoginInfo(String username, String password); //login validation from db
+		public String getParticipantNameById(int participantId);	//For mapping clientId to its Name for UI
+		public List<String> getAllParticipantName();
+//		public List<> getCorporateActionByParticipantId(Integer participant_id); //do it by joining of table
 	
 }
