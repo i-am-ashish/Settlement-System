f48b189abf8238249b26fdbf9f87edf6f50110b0
i-am-ashish <ashish1996nov@gmail.com>
2019-09-06 22:13:36 +0530

    Update SecurityDAO.java

--- Settlement System/src/com/dao/SecurityDAO.java
+++ Settlement System/src/com/dao/SecurityDAO.java
@@ -1,5 +1,18 @@
 package com.dao;
-jdfafd
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.pojo.Security;
+import com.pojo.Transaction;
+
 public interface SecurityDAO {
+	public List<String> getAllSecutityName();//for drop down in the ui
+	public HashMap<String,Integer> getSecurityIdMap();	
+	public HashMap<Integer,Integer> getCorporateAction();//it will return corporate_actoin and ratio
+	public boolean addCorporateAction(Security security); //add a new transaction to the table
+	public boolean updateCorporateAction(Security security); //update existing transaction
+	public boolean deleteCorporateAction(int securityId);//only do the corporsate_action as 0 and ratio as null dont delete the full security record
+	
 
 }
