0567a34c92529ab9f169d9825d4623a41be76698
i-am-ashish <ashish1996nov@gmail.com>
2019-09-06 22:24:13 +0530

    Update TransactionDAO.java

--- Settlement System/src/com/dao/TransactionDAO.java
+++ Settlement System/src/com/dao/TransactionDAO.java
@@ -8,6 +8,9 @@ import com.pojo.Transaction;
 public interface TransactionDAO {
 
 	public List<Transaction> getAllTransaction();
-	public List<Transaction> getTransactionByClientID(String participantId);
+	public List<Transaction> getTransactionByParticipantId(String participantId);
 	public boolean generateNettingTable(HashMap<Integer,HashMap<Integer,Integer>> hashMap);
+	public boolean addTransaction(Transaction transaction); //add a new transaction to the table
+	public boolean updateTransaction(Transaction transaction); //update existing transaction
+	public boolean deleteTransaction(int transactionId);
 }
